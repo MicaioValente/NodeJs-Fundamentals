51898c74c55d3b6567b335688b947c0da15e0a81
MicaioValente <micaiovalente@gmail.com>
2021-10-07 00:38:55 -0300

    feat(add post, put, patch, delete)

--- src/index.js
+++ src/index.js
@@ -6,4 +6,20 @@ app.get("/", (request, response) => {
     return response.json({ message: "aeeaeae" })
 })
 
+app.post("/frases", (request, response) => {
+    return response.json(["d boa", "eae", "opa"])
+})
+
+app.put("/frases/:id", (request, response) => {
+    return response.json(["feiao", "eae", "opa"])
+})
+
+app.patch("/frases/:id", (request, response) => {
+    return response.json(["feiao", "bonitao", "opa"])
+})
+
+
+app.delete("/frases/:id", (request, response) => {
+    return response.json(["feiao", "bonitao",])
+})
 app.listen(9999)
